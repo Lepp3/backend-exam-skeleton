aa7f5409c5a7dd85e4549f5aba98a3c62ac0403f
Lepp3 <lubaka05@gmail.com>
2025-02-09 13:33:53 +0200

    installed handlebars and set the usage path

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 import routes from './routes.js';
 
 const app = express();
+
+//handlebars setup
+app.set('view engine', 'handlebars');
+app.set('views', './src/views')
 
 //express setup
 app.use(express.static('src/public'));
